38fdc5a753e651459c0e7634e82373e43cea07bb
Joel Hughes <rudenoise@gmail.com>
2013-02-28 12:09:37 +0000

    add go implementation

--- go/parse.go
+++ go/parse.go
@@ -1,5 +1,11 @@
 package gb
 
+import(
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
 type Episode struct {
 	Title string
 	Body string
@@ -7,3 +13,20 @@ type Episode struct {
 }
 
 type Story []Episode
+
+func Parse (jsonBlob []byte) (Story, error) {
+	var story Story
+	err := json.Unmarshal(jsonBlob, &story)
+	return story, err
+}
+
+func GetEpisode (story Story, index int) (Episode, error) {
+	var episode Episode
+	var err error
+	if index > -1 && index < len(story) {
+		episode = story[index]
+	} else {
+		err = errors.New(fmt.Sprintf("Episode %d is not in this Story", index))
+	}
+	return episode, err
+}
--- go/parse_test.go
+++ go/parse_test.go
@@ -1,12 +1,51 @@
 package gb
 import(
 	"testing"
+	"fmt"
+	"errors"
 )
 
+var gbJSON = []byte(`
+	[{"title": "hi", "body": "first episode", "choices": [0]}]
+`)
+var testEpisode = Episode{"hi", "first episode", []float64{0}}
+
 func TestParse(t *testing.T) {
-	const gbJSON = `
-		[{"title": "hi", "body": "first episode", "choices": [0]}]
-	`
 	tGBStory := make(Story, 0)
-	tGBStory = append(tGBStory, Episode{"hi", "first episode", []float64{0}})
+	tGBStory = append(tGBStory, testEpisode)
+	expected := fmt.Sprintf("%s", tGBStory)
+	actualStory, err := Parse(gbJSON)
+	actualStr := fmt.Sprintf("%s", actualStory)
+	if err != nil {
+		t.Errorf("\n%s\n", err)
+	}
+	if expected != actualStr {
+		t.Errorf("\nExpected: %s\nActual: %s\n", expected, actualStr)
+	}
+}
+func TestGetEpisode(t *testing.T) {
+	expected := fmt.Sprintf("%s", testEpisode)
+	parsed, err := Parse(gbJSON)
+	if err != nil {
+		t.Errorf("\n%s\n", err)
+	}
+	episode, err := GetEpisode(parsed, 0)
+	if err != nil {
+		t.Errorf("\n%s\n", err)
+	}
+	actualEpisode := fmt.Sprintf("%s", episode)
+	if actualEpisode != expected {
+		t.Errorf("\nExpected: %s\nActual: %s\n", expected, actualEpisode)
+	}
+	invalidEpisode, err := GetEpisode(parsed, 1)
+	if err == nil {
+		t.Errorf("\nExpected an error to be raised as index is out of boundsn")
+	}
+	if invalidEpisode.Title != "" {
+		t.Errorf("\nExpected empty episode, got:\n%s\n", invalidEpisode)
+	}
+	expectedErr := errors.New("Episode 1 is not in this Story")
+	if fmt.Sprintf("%s", err) != fmt.Sprintf("%s", expectedErr) {
+		t.Errorf("\nBad error message:\n%s\nexpected:\n%s\n", err, expectedErr)
+	}
 }
