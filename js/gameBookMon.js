d4feabd592c2f3dc1d4b0b434f54240dc9383374
Rudenoise <rudenoise@gmail.com>
2013-06-23 22:18:53 +0100

    refactor to reduce superfluous API functions/steps

--- js/gameBookMon.js
+++ js/gameBookMon.js
@@ -1,25 +1,19 @@
 exports.gameBookMonad = (function () {
-    var gbm, read;
-    gbm = function (story) {
-        return read(story, 0)
-    };
-    read = function (story, index) {
+    var read = function (story, index) {
+        index = index || 0;
         var episode = {}, i, l, choice, title;
-        episode.read = function () {
-            return story[index].body;
-        };
         l = story[index].choices.length;
         for (i = 0; i < l; i += 1) {
             choice = story[index].choices[i];
             title = story[choice].title;
-            console.log(title, choice, i);
-            episode[title] = function () {
-                return read(story, choice)();
-            };
-            //episode[title] = read(story, choice);
+            episode[title] = (function (choice) {
+                return function () {
+                    return read(story, choice);
+                };
+            }(choice));
         }
         return episode;
     };
-    return gbm;
+    return read;
 }());
 
--- js/test/test-gameBookMon.js
+++ js/test/test-gameBookMon.js
@@ -3,11 +3,28 @@ var gbm = require('../gameBookMon.js').gameBookMonad,
 
 exports.stepOne = function (test) {
     var toString = Object.prototype.toString;
-    test.equal(toString.call(gbm), '[object Function]');
-    test.equal(toString.call(gbm(story).read), '[object Function]');
-    test.equal(gbm(story).read(), story[0].body);
-    test.equal(toString.call(gbm(story)['Wake up.']), '[object Function]');
-    // confused! work out api then flow, scope etc..
-    //test.equal(gbm(story).read()['Wake up.'], story[1].body);
+    test.equal(
+        toString.call(gbm),
+        '[object Function]',
+        'gbm is a function');
+    test.equal(
+        toString.call(gbm(story)),
+        '[object Object]',
+        'first story episode is an Object');
+    test.ok(
+        gbm(story).hasOwnProperty('Wake up.'),
+        'the object has the choices imprinted');
+    test.equal(
+        toString.call(gbm(story)['Wake up.']),
+        '[object Function]',
+        'keyed function for story choice');
+    test.ok(
+        gbm(story)['Wake up.']()
+            .hasOwnProperty('Jump out of the window.'),
+        'the object has the  first choice imprinted');
+    test.ok(
+        gbm(story)['Wake up.']()
+            .hasOwnProperty('Brush teeth.'),
+        'the object has the choices imprinted');
     test.done();
 };
