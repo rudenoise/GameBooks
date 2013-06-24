3003f62726ecddaa5d19029ccf9d20a83978f43f
rudenoise <rudenoise@gmail.com>
2013-06-24 16:18:41 +0100

    fix history behavior

--- js/gameBookMon.js
+++ js/gameBookMon.js
@@ -5,42 +5,60 @@ exports.gameBookMonad = (function () {
         replaceRE = new RegExp('[^a-z]', 'g'), 
         trimRE = new RegExp('[\_]{2,}|[\_]{1,}$', 'g');
     
-    read = function (story, index) {
+    read = function (story, index, previous) {
         // a named recursive function that returns
         // an object encapsulating state and
         // provides a chained API 
         
-        // default to story start point
+        // default current episode index to story start point
         index = index || 0;
-        
+
         // set-up varilables including
         // the episode object
-        var episode = {}, i, l, choice, title;
+        var episode = {}, l, choice, title;
         
         l = story[index].choices.length;
         
-        for (i = 0; i < l; i += 1) {
-            
+        while (l > 0) {
+            l -= 1;
             // loop episode options
-            choice = story[index].choices[i];
+            choice = story[index].choices[l];
             title = tokenise(story[choice].title);
-            
-            // decorate the episode with the next story options
-            episode[title] = function () {
-                // progress with story option
-                return read(story, choice);
-            };
+
+            // decorate the episode with the next story option
+            episode[title] = (function (story, choice, episode) {
+                // encapsulate scope
+                return function () {
+                    // progress with story option
+                    return read(story, choice, episode);
+                };
+            }(story, choice, episode));
         }
+
+        // provide a way to go back
+        episode.back = function () {
+            // default previous episode to story start point
+            return previous || read(story);
+        };
         return episode;
     };
     
     tokenise = function (string) {
-        // take a string and return a token string
-        // 'Oh hi.' -> 'oh-hi'
-        return string
+        // take a string and return a camel-cased token string
+        // 'Oh hi.' -> 'ohHi'
+        var i, l;
+        string = string
             .toLowerCase()
             .replace(replaceRE, '_')
-            .replace(trimRE, '');
+            .replace(trimRE, '').split('_');
+        l = string.length;
+        while (l > 1) {
+            l -= 1;
+            string[l] = string[l]
+                .charAt(0)
+                .toUpperCase() + string[l].slice(1);
+        }
+        return string.join('');
     };
     return read;
 }());
--- js/test/test-gameBookMon.js
+++ js/test/test-gameBookMon.js
@@ -12,23 +12,94 @@ exports.coreLoop = function (test) {
         '[object Object]',
         'first story episode is an Object');
     test.ok(
-        gbm(story).hasOwnProperty('wake_up'),
+        gbm(story).hasOwnProperty('wakeUp'),
         'the object has the choices imprinted');
     test.equal(
-        toString.call(gbm(story)['wake_up']),
+        toString.call(gbm(story)['wakeUp']),
         '[object Function]',
         'keyed function for story choice');
     test.ok(
-        gbm(story).wake_up()
-            .hasOwnProperty('jump_out_of_the_window'),
+        gbm(story).wakeUp()
+            .hasOwnProperty('jumpOutOfTheWindow'),
         'the object has the  first choice imprinted');
     test.ok(
-        gbm(story).wake_up()
-            .hasOwnProperty('brush_teeth'),
+        gbm(story).wakeUp()
+            .hasOwnProperty('brushTeeth'),
         'the object has the choices imprinted');
     test.ok(
-        gbm(story).wake_up().brush_teeth()
-            .hasOwnProperty('wake_up'),
+        gbm(story).wakeUp().brushTeeth()
+            .hasOwnProperty('wakeUp'),
         'the chained function calls recurse and hold state');
+    test.ok(
+        gbm(story)
+            .wakeUp()
+            .jumpOutOfTheWindow()
+            .hasOwnProperty('keepEyesClosed'),
+        '3 deep');
+    test.done();
+};
+
+exports.history = function (test) {
+    test.ok(
+        gbm(story).hasOwnProperty('back'),
+        'all episodes can go back');
+    test.ok(
+        gbm(story).back().hasOwnProperty('wakeUp'),
+        'calling back, at start, returns start');
+    test.ok(
+        gbm(story).wakeUp().hasOwnProperty('back'),
+        'all episodes can go back');
+    test.ok(
+        gbm(story)
+            .wakeUp()
+            .back()
+            .hasOwnProperty('wakeUp'),
+        'one forward, one back');
+    test.ok(
+        gbm(story)
+            .wakeUp()
+            .jumpOutOfTheWindow()
+            .back()
+            .hasOwnProperty('jumpOutOfTheWindow'),
+        'two forward, one back');
+    test.ok(
+        gbm(story)
+            .wakeUp()
+            .jumpOutOfTheWindow()
+            .keepEyesClosed()
+            .back()
+            .hasOwnProperty('keepEyesClosed'),
+        'three forward, two back');
+    test.ok(
+        gbm(story)
+            .wakeUp()
+            .jumpOutOfTheWindow()
+            .keepEyesClosed()
+            .back()
+            .back()
+            .hasOwnProperty('jumpOutOfTheWindow'),
+        'three forward, two back');
+    test.ok(
+        gbm(story)
+            .wakeUp()
+            .jumpOutOfTheWindow()
+            .keepEyesClosed()
+            .back()
+            .back()
+            .back()
+            .hasOwnProperty('wakeUp'),
+        'three forward, three back');
+    console.log(
+        gbm(story)
+            .wakeUp()
+            .brushTeeth()
+    );
+    test.ok(
+        gbm(story)// -> 0 A first...
+            .wakeUp()// 0 -> 1 Wake
+            .brushTeeth()// 0 -> 1 -> 3 Brush
+            .wakeUp()//  0 -> 1 -> 3 -> 1 Wake
+            .hasOwnProperty('jumpOutOfTheWindow'),
+        'a complicated path');
     test.done();
 };
